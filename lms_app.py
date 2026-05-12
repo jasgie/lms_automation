142fa64babb43fa97eae33c476f67e42226661b3
John Denzel Diaz Tomayao <85047077+jasgie@users.noreply.github.com>
2026-05-12 10:47:03 +0800

    Bump version to 1.1.9, update binary files, and include new base library

--- lms_app.py
+++ lms_app.py
@@ -42,7 +42,7 @@ os.environ.setdefault("PYTHONIOENCODING", "utf-8")
 # Version  (bump this string when distributing a new build)
 # ─────────────────────────────────────────────────────────────────────────────
 
-VERSION = "1.1.8"
+VERSION = "1.1.9"
 
 # GitHub repo used for update checks (format: "owner/repo")
 GITHUB_REPO = "jasgie/lms_automation"
